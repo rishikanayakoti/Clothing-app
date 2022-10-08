6effc1eb24977127f220a9ab4dccae4f1f5755af
Rishika <rishika.nayakoti3781@gmail.com>
2022-10-08 11:43:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import About from './components/About';
+import Contact from './components/Contact';
+import clickingacard from './components/clickingacard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet"></link>
+     <Routes>
+        {/* route for home  */}
+        <Route path="" element={<Home />} />
+
+        {/* route for podcasts */}
+        <Route path="Blog" element={<Blog />} />
+
+        {/* route for signin */}
+        <Route path="About" element={<About/>}/>
+
+        {/* route for TVshows */}
+        <Route path="Contact" element={<Contact/>} />
+      </Routes>
+      </div>
   );
 }
 
